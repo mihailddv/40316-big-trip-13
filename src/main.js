38300af745b5100351781e211b68c13ad1289bc2
mihail <mihailddv@gmail.com>
2020-11-17 23:58:16 +0300

    render - added header

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import {createTripInfo} from "./view/trip-info";
+import {createTripTabs} from "./view/trip-tabs";
+import {createTripFilter} from "./view/trip-filter";
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -6,5 +8,8 @@ const render = (container, template, place) => {
 
 const siteMainElement = document.querySelector(`.page-body`);
 const siteTripMainElement = siteMainElement.querySelector(`.trip-main`);
+const siteTripControlsElement = siteTripMainElement.querySelector(`.trip-controls`);
 
 render(siteTripMainElement, createTripInfo(), `afterbegin`);
+render(siteTripControlsElement, createTripTabs(), `afterbegin`);
+render(siteTripControlsElement, createTripFilter(), `beforeend`);
