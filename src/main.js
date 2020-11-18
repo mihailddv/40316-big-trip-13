b656d81cad24de672d25bbba7b9060034c0fb9a2
mihail <mihailddv@gmail.com>
2020-11-18 23:29:20 +0300

    render - added list

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import {createTripInfo} from "./view/trip-info";
 import {createTripTabs} from "./view/trip-tabs";
 import {createTripFilter} from "./view/trip-filter";
 import {createLoading} from "./view/loading";
-import {createPointsList} from "./view/route-points";
+import {createList} from "./view/list";
+import {createListEmpty} from "./view/list-empty";
+import {createListFilter} from "./view/list-filter";
+import {createListSort} from "./view/list-sort";
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -16,5 +19,8 @@ const siteTripEventsElement = siteMainElement.querySelector(`.trip-events`);
 render(siteTripMainElement, createTripInfo(), `afterbegin`);
 render(siteTripControlsElement, createTripTabs(), `afterbegin`);
 render(siteTripControlsElement, createTripFilter(), `beforeend`);
-render(siteTripEventsElement, createLoading(), `afterbegin`);
-render(siteTripEventsElement, createPointsList(), `afterbegin`);
+render(siteTripEventsElement, createList(), `afterbegin`);
+render(siteTripEventsElement, createListFilter(), `beforeend`);
+render(siteTripEventsElement, createListSort(), `beforeend`);
+render(siteTripEventsElement, createListEmpty(), `beforeend`);
+render(siteTripEventsElement, createLoading(), `beforeend`);
--- src/view/list-sort.js
+++ src/view/list-sort.js
@@ -1,32 +1,62 @@
-export const createPointsList = () => {
+export const createListSort = () => {
   return /* html*/ `<ul class="trip-events__list">
     <li class="trip-events__item">
       <div class="event">
         <time class="event__date" datetime="2019-03-18">MAR 18</time>
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/check-in.png" alt="Event type icon">
         </div>
-        <h3 class="event__title">Taxi Amsterdam</h3>
+        <h3 class="event__title">Check-in Chamonix</h3>
         <div class="event__schedule">
           <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            <time class="event__start-time" datetime="2019-03-18T12:25">16:20</time>
             &mdash;
-            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+            <time class="event__end-time" datetime="2019-03-18T13:35">17:00</time>
           </p>
-          <p class="event__duration">30M</p>
+          <p class="event__duration">40M</p>
         </div>
         <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">20</span>
+          &euro;&nbsp;<span class="event__price-value">600</span>
         </p>
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">
           <li class="event__offer">
-            <span class="event__offer-title">Order Uber</span>
+            <span class="event__offer-title">Add breakfast</span>
             &plus;&euro;&nbsp;
-            <span class="event__offer-price">20</span>
+            <span class="event__offer-price">50</span>
           </li>
         </ul>
         <button class="event__favorite-btn event__favorite-btn--active" type="button">
+          <span class="visually-hidden">Add to favorite</span>
+          <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+            <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+          </svg>
+        </button>
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
+      </div>
+    </li>
+
+    <li class="trip-events__item">
+      <div class="event">
+        <time class="event__date" datetime="2019-03-20">MAR 20</time>
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/sightseeing.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">Sightseeing Geneva</h3>
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="2019-03-20T11:15">11:15</time>
+            &mdash;
+            <time class="event__end-time" datetime="2019-03-20T12:15">12:15</time>
+          </p>
+          <p class="event__duration">1H</p>
+        </div>
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">180</span>
+        </p>
+        <button class="event__favorite-btn" type="button">
           <span class="visually-hidden">Add to favorite</span>
           <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -121,44 +151,6 @@ export const createPointsList = () => {
 
     <li class="trip-events__item">
       <div class="event">
-        <time class="event__date" datetime="2019-03-18">MAR 18</time>
-        <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/check-in.png" alt="Event type icon">
-        </div>
-        <h3 class="event__title">Check-in Chamonix</h3>
-        <div class="event__schedule">
-          <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-18T12:25">16:20</time>
-            &mdash;
-            <time class="event__end-time" datetime="2019-03-18T13:35">17:00</time>
-          </p>
-          <p class="event__duration">40M</p>
-        </div>
-        <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">600</span>
-        </p>
-        <h4 class="visually-hidden">Offers:</h4>
-        <ul class="event__selected-offers">
-          <li class="event__offer">
-            <span class="event__offer-title">Add breakfast</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">50</span>
-          </li>
-        </ul>
-        <button class="event__favorite-btn event__favorite-btn--active" type="button">
-          <span class="visually-hidden">Add to favorite</span>
-          <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
-            <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
-          </svg>
-        </button>
-        <button class="event__rollup-btn" type="button">
-          <span class="visually-hidden">Open event</span>
-        </button>
-      </div>
-    </li>
-
-    <li class="trip-events__item">
-      <div class="event">
         <time class="event__date" datetime="2019-03-19">MAR 19</time>
         <div class="event__type">
           <img class="event__type-icon" width="42" height="42" src="img/icons/sightseeing.png" alt="Event type icon">
@@ -189,6 +181,44 @@ export const createPointsList = () => {
           </li>
         </ul>
         <button class="event__favorite-btn" type="button">
+          <span class="visually-hidden">Add to favorite</span>
+          <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+            <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+          </svg>
+        </button>
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
+      </div>
+    </li>
+
+    <li class="trip-events__item">
+      <div class="event">
+        <time class="event__date" datetime="2019-03-18">MAR 18</time>
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">Taxi Amsterdam</h3>
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            &mdash;
+            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          </p>
+          <p class="event__duration">30M</p>
+        </div>
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">20</span>
+        </p>
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          <li class="event__offer">
+            <span class="event__offer-title">Order Uber</span>
+            &plus;&euro;&nbsp;
+            <span class="event__offer-price">20</span>
+          </li>
+        </ul>
+        <button class="event__favorite-btn event__favorite-btn--active" type="button">
           <span class="visually-hidden">Add to favorite</span>
           <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -290,36 +320,6 @@ export const createPointsList = () => {
         </div>
         <p class="event__price">
           &euro;&nbsp;<span class="event__price-value">20</span>
-        </p>
-        <button class="event__favorite-btn" type="button">
-          <span class="visually-hidden">Add to favorite</span>
-          <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
-            <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
-          </svg>
-        </button>
-        <button class="event__rollup-btn" type="button">
-          <span class="visually-hidden">Open event</span>
-        </button>
-      </div>
-    </li>
-
-    <li class="trip-events__item">
-      <div class="event">
-        <time class="event__date" datetime="2019-03-20">MAR 20</time>
-        <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/sightseeing.png" alt="Event type icon">
-        </div>
-        <h3 class="event__title">Sightseeing Geneva</h3>
-        <div class="event__schedule">
-          <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-20T11:15">11:15</time>
-            &mdash;
-            <time class="event__end-time" datetime="2019-03-20T12:15">12:15</time>
-          </p>
-          <p class="event__duration">1H</p>
-        </div>
-        <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">180</span>
         </p>
         <button class="event__favorite-btn" type="button">
           <span class="visually-hidden">Add to favorite</span>
