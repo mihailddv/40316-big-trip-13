b4aea77b8ba614aa5cef75cd1842de6717b57d54
mihail <mihailddv@gmail.com>
2020-11-18 23:56:15 +0300

    render - added AddNewPoint

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import {createList} from "./view/list";
 import {createListEmpty} from "./view/list-empty";
 import {createListFilter} from "./view/list-filter";
 import {createListSort} from "./view/list-sort";
+import {createTripSort} from "./view/trip-sort";
+import {createAddNewPoint} from './view/add-new-point';
+import {createAddNewPointWithoutOffers} from './view/add-new-point-without-offers';
+import {createAddNewPointWithoutDesctination} from './view/add-new-point-without-destination';
+import {createEditPoint} from './view/edit-point';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -19,7 +24,12 @@ const siteTripEventsElement = siteMainElement.querySelector(`.trip-events`);
 render(siteTripMainElement, createTripInfo(), `afterbegin`);
 render(siteTripControlsElement, createTripTabs(), `afterbegin`);
 render(siteTripControlsElement, createTripFilter(), `beforeend`);
-render(siteTripEventsElement, createList(), `afterbegin`);
+render(siteTripEventsElement, createTripSort(), `afterbegin`);
+render(siteTripEventsElement, createAddNewPoint(), `beforeend`);
+render(siteTripEventsElement, createAddNewPointWithoutOffers(), `beforeend`);
+render(siteTripEventsElement, createAddNewPointWithoutDesctination(), `beforeend`);
+render(siteTripEventsElement, createEditPoint(), `beforeend`);
+render(siteTripEventsElement, createList(), `beforeend`);
 render(siteTripEventsElement, createListFilter(), `beforeend`);
 render(siteTripEventsElement, createListSort(), `beforeend`);
 render(siteTripEventsElement, createListEmpty(), `beforeend`);
