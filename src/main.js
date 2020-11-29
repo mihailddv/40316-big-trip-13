eabab9763821ab6d13c797c03027c53eabe8bfdd
mihail <mihailddv@gmail.com>
2020-11-29 13:24:08 +0300

    main - TripInfoView, TripTabsView, TripFilterView

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import {createTripInfo} from './view/trip-info';
-import {createTripTabs} from './view/trip-tabs';
-import {createTripFilter} from './view/trip-filter';
+import TripInfoView from './view/trip-info';
+import TripTabsView from './view/trip-tabs';
+import TripFilterView from './view/trip-filter';
 // import {createLoading} from './view/loading';
 import {createList} from './view/list';
 // import {createListEmpty} from './view/list-empty';
@@ -9,7 +9,7 @@ import {createTripSort} from './view/trip-sort';
 import {createEditPoint} from './view/edit-point';
 import {createPoint} from './view/point';
 import {generateEvent} from '../mock/event';
-import {renderTemplate, calculateTotal} from './utils';
+import {renderTemplate, calculateTotal, renderElement, RenderPosition} from './utils';
 // import {generateFilter} from '../mock/filter';
 
 const EVENT_COUNT = 20;
@@ -21,9 +21,12 @@ const siteTripMainElement = siteMainElement.querySelector(`.trip-main`);
 const siteTripControlsElement = siteTripMainElement.querySelector(`.trip-controls`);
 const siteTripEventsElement = siteMainElement.querySelector(`.trip-events`);
 
-renderTemplate(siteTripMainElement, createTripInfo(), `afterbegin`);
-renderTemplate(siteTripControlsElement, createTripTabs(), `afterbegin`);
-renderTemplate(siteTripControlsElement, createTripFilter(), `beforeend`);
+// renderTemplate(siteTripMainElement, createTripInfo(), `afterbegin`);
+renderElement(siteTripMainElement, new TripInfoView().getElement(), RenderPosition.AFTERBEGIN);
+renderElement(siteTripControlsElement, new TripTabsView().getElement(), RenderPosition.AFTERBEGIN);
+// renderTemplate(siteTripControlsElement, createTripTabs(), `afterbegin`);
+renderElement(siteTripControlsElement, new TripFilterView().getElement(), RenderPosition.BEFOREEND);
+// renderTemplate(siteTripControlsElement, createTripFilter(), `beforeend`);
 renderTemplate(siteTripEventsElement, createTripSort(), `afterbegin`);
 renderTemplate(siteTripEventsElement, createList(), `beforeend`);
 
--- src/view/trip-filter.js
+++ src/view/trip-filter.js
@@ -1,4 +1,6 @@
-export const createTripFilter = () => {
+import {createElement} from "../utils.js";
+
+const createTripFilterTemplate = () => {
   return /* html */ `<form class="trip-filters" action="#" method="get">
     <div class="trip-filters__filter">
       <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
@@ -18,3 +20,25 @@ export const createTripFilter = () => {
     <button class="visually-hidden" type="submit">Accept filter</button>
   </form>`;
 };
+
+export default class TripInfo {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTripFilterTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
