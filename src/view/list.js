b85bad7391940ed7514b698770c8f218b634a411
mihail <mihailddv@gmail.com>
2020-12-04 21:48:50 +0300

    Abstract - update simple components

--- src/view/list.js
+++ src/view/list.js
@@ -1,26 +1,10 @@
-import {createElement} from "../utils.js";
+import AbstractView from "./abstract.js";
 
 const createList = () => {
   return /* html */ `<ul class="trip-events__list"></ul>`;
 };
-export default class List {
-  constructor() {
-    this._element = null;
-  }
-
+export default class List extends AbstractView {
   getTemplate() {
     return createList();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
