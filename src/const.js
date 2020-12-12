c349b77d1d9067a80aeac7bd9428ab55a0bfb2d5
mihail <mihailddv@gmail.com>
2020-12-12 16:42:55 +0300

    sort - added SortType

--- src/const.js
+++ src/const.js
@@ -60,3 +60,13 @@ export const EVENT_TYPE = [
     image: `restaurant`,
   },
 ];
+
+export const SortType = {
+  DEFAULT: `default`,
+  DATE_DOWN: `date-down`,
+  DATE_UP: `date-up`,
+  TIME_DOWN: `time-down`,
+  TIME_UP: `time-up`,
+  PRICE_DOWN: `price-down`,
+  PRICE_UP: `price-up`,
+};
