9a5f6087eb503de2dd3d279a47c59d8c76bde5a3
mihail <mihailddv@gmail.com>
2021-01-12 21:28:26 +0300

    fixed save

--- src/view/edit-point.js
+++ src/view/edit-point.js
@@ -65,29 +65,30 @@ export const createEditPointTemplate = (data, destinations, offers) => {
     const names = Object.values(offers).map((item) => item);
     const type = names.find((offer) => offer.type === data.type);
 
-    const list = type.offers.slice().map((offer) => {
-      const isChecked = eventType.offers.some((item) => item.title === offer.title);
+    if (type) {
+      const list = type.offers.slice().map((offer) => {
+        const isChecked = eventType.offers.some((item) => item.title === offer.title);
 
-      return /* html */`
-        <div class="event__offer-selector">
-          <input
-            class="event__offer-checkbox visually-hidden"
-            id="event-offer-${offer.title}"
-            type="checkbox"
-            name="event-offer-${offer.title}"
-            data-name="${offer.title}"
-            ${isChecked ? `checked` : ``}
-          >
-          <label class="event__offer-label" for="event-offer-${offer.title}">
-            <span class="event__offer-title">${offer.title}</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">${price}</span>
-          </label>
-        </div>
-      `;
-    });
-
-    return list.join(``);
+        return /* html */`
+          <div class="event__offer-selector">
+            <input
+              class="event__offer-checkbox visually-hidden"
+              id="event-offer-${offer.title}"
+              type="checkbox"
+              name="event-offer-${offer.title}"
+              data-name="${offer.title}"
+              ${isChecked ? `checked` : ``}
+            >
+            <label class="event__offer-label" for="event-offer-${offer.title}">
+              <span class="event__offer-title">${offer.title}</span>
+              &plus;&euro;&nbsp;
+              <span class="event__offer-price">${price}</span>
+            </label>
+          </div>
+        `;
+      });
+      return list.join(``);
+    }
   };
 
 
